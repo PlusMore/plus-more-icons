efb31a8463a6fc56391e5478ebd0377468afbb41
Patrick Scott <patrick.lee.scott@gmail.com>
2014-05-28 14:59:12 -0400

    load fontastic icons from cdn instead of hosting

--- package.js
+++ package.js
@@ -2,18 +2,22 @@ Package.describe({
   summary: "Plus more icons"
 });
 
-Package.on_use(function (api) {
-  api.use(['less'], 'client');
+// Package.on_use(function (api) {
+//   api.use(['less'], 'client');
 
-  // icons
-  api.add_files([
-    'plus-more-icons/plus-more-icons.eot',
-    'plus-more-icons/plus-more-icons.woff',
-    'plus-more-icons/plus-more-icons.ttf',
-    'plus-more-icons/plus-more-icons.svg'
-  ], "client", {isAsset: true});
+//   // icons
+//   api.add_files([
+//     'plus-more-icons/plus-more-icons.eot',
+//     'plus-more-icons/plus-more-icons.woff',
+//     'plus-more-icons/plus-more-icons.ttf',
+//     'plus-more-icons/plus-more-icons.svg'
+//   ], "client", {isAsset: true});
 
-  api.add_files([
-    'plus-more-icons/plus-more-icons.less'
-  ], 'client')
+//   api.add_files([
+//     'plus-more-icons/plus-more-icons.less'
+//   ], 'client')
+// });
+
+Package.on_use(function (api){
+  api.add_files('load.js', 'client');
 });
